7d5599e7d1780db0f2be79c423ac502e5c13071f
Martí Arnau <marnauortega@gmail.com>
2023-03-28 08:31:20 +0200

    feat: success screen description and row

--- packages/app/features/addParcelForm/SuccessScreen.jsx
+++ packages/app/features/addParcelForm/SuccessScreen.jsx
@@ -1,3 +1,8 @@
+import { useContext } from 'react'
+import { DaysContext } from 'app/provider/DaysProvider'
+import { AddParcelContext } from 'app/provider/AddParcelProvider'
+import parcels from 'app/data/parcels'
+
 import Header from 'app/components/Header'
 import { useSx, View } from 'dripsy'
 
@@ -5,10 +10,20 @@ import AppButton from 'app/components/AppButton'
 import ParcelRow from 'app/components/ParcelRow'
 
 import { useRouter } from 'solito/router'
+import FieldDescription from 'app/components/FieldDescription'
+import checkIcon from 'app/assets/images/checkIcon.svg'
 
 const SuccessScreen = () => {
+  const { id } = useContext(AddParcelContext)
+  const { dayList } = useContext(DaysContext)
   const sx = useSx()
   const { push } = useRouter()
+
+  const parcel = parcels.find((parcel) => parcel.id.$oid === id)
+  const day = dayList.find((day) =>
+    day.parcels.find((parcel) => parcel.$oid === id)
+  )
+  const parcelRef = day?.parcels.find((parcel) => parcel.$oid === id)
 
   return (
     <>
@@ -18,10 +33,18 @@ const SuccessScreen = () => {
           flex: 1,
           justifyContent: 'center',
           alignItems: 'center',
-          padding: 20,
         }}
       >
-        <ParcelRow />
+        <FieldDescription sx={{ padding: 20 }} icon={checkIcon}>
+          Parcel added successfully
+        </FieldDescription>
+        <ParcelRow
+          id={id}
+          carrier={parcelRef?.carrier}
+          itemsCount={parcel?.itemsCount}
+          deliveryStatus={parcelRef?.deliveryStatus}
+          daySlug
+        />
       </View>
       <View sx={{ padding: 20 }}>
         <AppButton
